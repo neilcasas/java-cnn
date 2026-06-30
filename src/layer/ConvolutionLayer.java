d14729d6ef9adb75bf193af6385a2ccd8d594115
vgr <neilalfonzcasas@gmail.com>
2026-06-30 20:46:12 +0800

    finish convolution backpropagation

--- src/layer/ConvolutionLayer.java
+++ src/layer/ConvolutionLayer.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static data.MatrixUtility.add;
+import static data.MatrixUtility.multiply;
+
 public class ConvolutionLayer extends Layer {
     private long SEED;
 
@@ -15,7 +18,11 @@ public class ConvolutionLayer extends Layer {
     private int _inRows;
     private int _inCols;
 
-    public ConvolutionLayer(long SEED, List<double[][]> _filters, int _filterSize, int _stepSize, int _inLength, int _inRows, int _inCols, int numFilters) {
+    private List<double[][]> _lastInput;
+
+    private double _learningRate;
+
+    public ConvolutionLayer(long SEED, List<double[][]> _filters, int _filterSize, int _stepSize, int _inLength, int _inRows, int _inCols, int numFilters, double learningRate) {
         this.SEED = SEED;
         this._filters = _filters;
         this._filterSize = _filterSize;
@@ -23,6 +30,7 @@ public class ConvolutionLayer extends Layer {
         this._inLength = _inLength;
         this._inRows = _inRows;
         this._inCols = _inCols;
+        _learningRate = learningRate;
 
         generateRandomFilters(numFilters);
     }
@@ -47,6 +55,8 @@ public class ConvolutionLayer extends Layer {
     }
 
     public List<double[][]> convolutionForwardPass(List<double[][]> list) {
+        _lastInput = list;
+
         List<double[][]> output = new ArrayList<>();
 
         for (int m = 0; m < list.size(); m++) {
@@ -98,6 +108,25 @@ public class ConvolutionLayer extends Layer {
         return output;
     }
 
+    public double[][] spaceArray(double[][] input) {
+        if (_stepSize == 1) {
+            return input;
+        }
+
+        int outRows = (input.length - 1) * _stepSize + 1;
+        int outCols = (input[0].length - 1) * _stepSize + 1;
+
+        double[][] output = new double[outRows][outCols];
+
+        for(int i = 0 ; i < input.length; i++) {
+            for (int j = 0; j < input[0].length; j++) {
+                output[i * _stepSize][j* _stepSize] = input[i][j];
+            }
+        }
+
+        return output;
+    }
+
     @Override
     public double[] getOutput(List<double[][]> input) {
         List<double[][]> output = convolutionForwardPass(input);
@@ -113,12 +142,123 @@ public class ConvolutionLayer extends Layer {
 
     @Override
     public void backPropagation(double[] dLd0) {
-
+        List<double[][]> matrixInput = vectorToMatrix(dLd0, _inLength, _inRows, _inCols);
+        backPropagation(matrixInput);
     }
 
     @Override
-    public void backPropagation(List<double[][]> dld0) {
+    public void backPropagation(List<double[][]> dldO) {
+        List<double[][]> filtersDelta = new ArrayList<>();
+        List<double[][]> dLdOPreviousLayer = new ArrayList<>();
 
+
+        for (int f = 0; f < _filters.size(); f++) {
+            filtersDelta.add(new double[_filterSize][_filterSize]);
+        }
+
+        for(int i = 0; i < _lastInput.size(); i++) {
+            double[][] errorForInput = new double[_inRows][_inCols];
+
+            for (int f = 0; f < _filters.size(); f++) {
+                double[][] currFilter = _filters.get(f);
+                double[][] error = dldO.get(i * _filters.size() + f);
+
+                double[][] spacedError = spaceArray(error);
+                double[][] dLdF = convolve(_lastInput.get(i), spacedError, 1);
+
+                double[][] delta = multiply(dLdF, _learningRate);
+                double[][] newTotalDelta = add(filtersDelta.get(f), delta);
+
+                filtersDelta.set(f, newTotalDelta);
+
+                double[][] flippedError = flipArrayHorizontal(flipArrayVertical(spacedError));
+                errorForInput = add(errorForInput, fullConvolve(currFilter, flippedError));
+
+            }
+            dLdOPreviousLayer.add(errorForInput);
+        }
+
+        for(int f = 0; f < _filters.size(); f++) {
+            double[][] modified = add(filtersDelta.get(f), _filters.get(f));
+            _filters.set(f, modified);
+        }
+
+        if(_previousLayer != null) {
+            _previousLayer.backPropagation(dLdOPreviousLayer);
+        }
+    }
+
+    public double[][] flipArrayHorizontal(double[][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
+
+        double[][] output = new double[rows][cols];
+
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0 ; j < cols; j++) {
+                output[rows-i-1][j] = array[i][j];
+            }
+        }
+
+        return output;
+    }
+
+    public double[][] flipArrayVertical(double[][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
+
+        double[][] output = new double[rows][cols];
+
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0 ; j < cols; j++) {
+                output[i][cols-j-1] = array[i][j];
+            }
+        }
+
+        return output;
+    }
+
+    private double[][] fullConvolve(double[][] input, double[][] filter) {
+        int outRows = (input.length + filter.length) + 1;
+        int outCols = (input[0].length + filter[0].length) + 1;
+
+        int inRows = input.length;
+        int inCols = input[0].length;
+
+        int fRows = filter.length;
+        int fCols = filter[0].length;
+
+        double[][] output = new double[outRows][outCols];
+
+        int outRow = 0;
+        int outCol;
+
+        for(int i = -fRows + 1; i < inRows; i++) {
+            outCol = 0;
+
+            for(int j = -fCols + 1; j < inCols; j++) {
+                double sum = 0.0;
+
+                // apply filter around this position
+                for(int x = 0; x < fRows; x++) {
+                    for (int y = 0; y < fCols; y++) {
+                        int inputRowIndex = i + x;
+                        int inputColIndex = j + y;
+
+                        if (inputRowIndex >= 0 && inputColIndex >= 0 && inputRowIndex < inRows && inputColIndex < inCols) {
+                            double value = filter[x][y] * input[inputRowIndex][inputColIndex];
+                            sum += value;
+                        }
+                    }
+                }
+
+                output[outRow][outCol] = sum;
+                outCol++;
+            }
+            outRow++;
+        }
+
+        return output;
     }
 
     @Override
